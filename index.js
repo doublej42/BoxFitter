786b11923b928242baafc0345cb92a55937a5e1c
Jeff Jacob <phansoft@gmail.com>
2023-11-05 14:01:10 -0800

    Can now add a first box and it will scale

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ var parentList = [];
 
 function addBox(event) {
     console.log("Adding new box");
-    const newBox = document.createElement("div");
-    newBox.classList.add("box");
-    newBox.classList.add("parent");
+    if (parentList.length == 0)
+    {
+        parentList.push(document.getElementById("pageContainer"));
+    }
+    parentList.forEach((item) => {
+        
+        const newBox = document.createElement("div");
+        newBox.id = "t1";
+        newBox.classList.add("box");
+        newBox.classList.add("parent");
+        newBox.dataset.width = document.getElementById("width").value;
+        newBox.dataset.height = document.getElementById("height").value;
+        newBox.dataset.name = document.getElementById("name").value;
+        var parentWidth, parentHeight;
+        if ("width" in item.dataset)
+        {
+            //todo    
+        }
+        else
+        {   
+            parentWidth = item.clientWidth;
+            parentHeight = item.clientHeight;
+        }
+        newBox.dataset.scale = Math.min(parentWidth/newBox.dataset.width,parentHeight/newBox.dataset.height);
+        
+        newBox.innerHTML = newBox.dataset.scale;
+        item.appendChild(newBox);
+        newBox.style.width = (newBox.dataset.width * newBox.dataset.scale) + "px";
+        newBox.style.height = (newBox.dataset.height * newBox.dataset.scale) + "px";
+    });
 }
 
 function selectParent(event) {
@@ -35,7 +62,6 @@ function updateSelectedList() {
         newLi.innerHTML = item.dataset.name
         console.log(ol, newLi);
         ol.appendChild(newLi);
-
     });
 }
 
